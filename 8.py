53e1adaf79349ea00d6e3202de2bf2959326a8d4
Baibhav Ojha <36107094+StarSpider@users.noreply.github.com>
2019-07-15 15:06:57 +0530

    Update and rename 8.py to 81.py

--- 8.py
+++
@@ -1,5 +0,0 @@
-val=input()
-if(val%2==0):
-    print("Even")
-else:
-    print("Odd")
---
+++ 81.py
@@ -0,0 +1,8 @@
+val=int(input())
+if(val>0):
+    if(val%2!=0):
+        print("Odd")
+    else:
+        print("Even")
+else:
+    print("Invalid")
