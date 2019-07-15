ef615983bb6ae34f151fe48a804740ad33f62024
Baibhav Ojha <36107094+StarSpider@users.noreply.github.com>
2019-07-15 14:39:57 +0530

    Update and rename 711.py to 7111.py

--- 711.py
+++
@@ -1,7 +0,0 @@
-_var=int(input())
-if(_var>=1):
-    print("Positive")
-elif(_var<=-1):
-    print("Negative")
-else:
-    print("Zero")
---
+++ 7111.py
@@ -0,0 +1,7 @@
+val=input()
+if(val>=1):
+    print("Positive")
+elif(val==0):
+    print("Zero")
+else:
+    print("Negative")
